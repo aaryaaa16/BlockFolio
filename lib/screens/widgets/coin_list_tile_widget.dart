6cd3f0d37391d8f04f421c6df87e0a23a8b3b1f7
crpatil1901 <crpatil1901@gmail.com>
2023-05-02 23:15:35 +0530

    Card Removed

--- lib/screens/widgets/coin_list_tile_widget.dart
+++ lib/screens/widgets/coin_list_tile_widget.dart
@@ -10,65 +10,63 @@ class CoinListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.network(
-              coin.image,
-              height: 48,
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Image.network(
+            coin.image,
+            height: 48,
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              coin.name,
             ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                coin.name,
-              ),
-              Text(
-                coin.symbol.toUpperCase(),
-              )
-            ],
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  Text(
-                    formatCurrency.format(coin.currentPrice),
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(
-                        coin.priceChangePercentage24H >= 0
-                            ? Icons.arrow_upward_rounded
-                            : Icons.arrow_downward_rounded,
+            Text(
+              coin.symbol.toUpperCase(),
+            )
+          ],
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Text(
+                  formatCurrency.format(coin.currentPrice),
+                ),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Icon(
+                      coin.priceChangePercentage24H >= 0
+                          ? Icons.arrow_upward_rounded
+                          : Icons.arrow_downward_rounded,
+                      color: coin.priceChangePercentage24H >= 0
+                          ? Colors.green
+                          : Colors.red,
+                      size: 14,
+                    ),
+                    Text(
+                      coin.priceChangePercentage24H.abs().toStringAsFixed(2) +
+                          "%",
+                      style: TextStyle(
                         color: coin.priceChangePercentage24H >= 0
                             ? Colors.green
                             : Colors.red,
-                        size: 14,
                       ),
-                      Text(
-                        coin.priceChangePercentage24H.abs().toStringAsFixed(2) +
-                            "%",
-                        style: TextStyle(
-                          color: coin.priceChangePercentage24H >= 0
-                              ? Colors.green
-                              : Colors.red,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                    ),
+                  ],
+                )
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 }
