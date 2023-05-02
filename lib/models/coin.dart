f4b91d06a1eeb9449a4e10b2759b093c9723b3f8
crpatil1901 <crpatil1901@gmail.com>
2023-05-02 20:18:13 +0530

    Added test coin

--- lib/models/coin.dart
+++ lib/models/coin.dart
@@ -1,65 +1,65 @@
 import 'dart:convert';
 
 class Coin {
-    String id;
-    String symbol;
-    String name;
-    String image;
-    double currentPrice;
-    int marketCap;
-    int marketCapRank;
-    int? fullyDilutedValuation;
-    double totalVolume;
-    double high24H;
-    double low24H;
-    double priceChange24H;
-    double priceChangePercentage24H;
-    double marketCapChange24H;
-    double marketCapChangePercentage24H;
-    double circulatingSupply;
-    double? totalSupply;
-    double? maxSupply;
-    double ath;
-    double athChangePercentage;
-    DateTime athDate;
-    double atl;
-    double atlChangePercentage;
-    DateTime atlDate;
-    DateTime lastUpdated;
+  String id;
+  String symbol;
+  String name;
+  String image;
+  double currentPrice;
+  int marketCap;
+  int marketCapRank;
+  int? fullyDilutedValuation;
+  double totalVolume;
+  double high24H;
+  double low24H;
+  double priceChange24H;
+  double priceChangePercentage24H;
+  double marketCapChange24H;
+  double marketCapChangePercentage24H;
+  double circulatingSupply;
+  double? totalSupply;
+  double? maxSupply;
+  double ath;
+  double athChangePercentage;
+  DateTime athDate;
+  double atl;
+  double atlChangePercentage;
+  DateTime atlDate;
+  DateTime lastUpdated;
 
-    Coin({
-        required this.id,
-        required this.symbol,
-        required this.name,
-        required this.image,
-        required this.currentPrice,
-        required this.marketCap,
-        required this.marketCapRank,
-        this.fullyDilutedValuation,
-        required this.totalVolume,
-        required this.high24H,
-        required this.low24H,
-        required this.priceChange24H,
-        required this.priceChangePercentage24H,
-        required this.marketCapChange24H,
-        required this.marketCapChangePercentage24H,
-        required this.circulatingSupply,
-        this.totalSupply,
-        this.maxSupply,
-        required this.ath,
-        required this.athChangePercentage,
-        required this.athDate,
-        required this.atl,
-        required this.atlChangePercentage,
-        required this.atlDate,
-        required this.lastUpdated,
-    });
+  Coin({
+    required this.id,
+    required this.symbol,
+    required this.name,
+    required this.image,
+    required this.currentPrice,
+    required this.marketCap,
+    required this.marketCapRank,
+    this.fullyDilutedValuation,
+    required this.totalVolume,
+    required this.high24H,
+    required this.low24H,
+    required this.priceChange24H,
+    required this.priceChangePercentage24H,
+    required this.marketCapChange24H,
+    required this.marketCapChangePercentage24H,
+    required this.circulatingSupply,
+    this.totalSupply,
+    this.maxSupply,
+    required this.ath,
+    required this.athChangePercentage,
+    required this.athDate,
+    required this.atl,
+    required this.atlChangePercentage,
+    required this.atlDate,
+    required this.lastUpdated,
+  });
 
-    factory Coin.fromRawJson(String str) => Coin.fromJson(json.decode(str));
+  factory Coin.fromRawJson(String str) => Coin.fromJson(json.decode(str));
 
-    String toRawJson() => json.encode(toJson());
+  String toRawJson() => json.encode(toJson());
 
-    factory Coin.fromJson(Map<String, dynamic> json) => Coin(
+  factory Coin.fromJson(Map<String, dynamic> json) => Coin(
         id: json["id"],
         symbol: json["symbol"],
         name: json["name"],
@@ -72,9 +72,11 @@ class Coin {
         high24H: json["high_24h"]?.toDouble(),
         low24H: json["low_24h"]?.toDouble(),
         priceChange24H: json["price_change_24h"]?.toDouble(),
-        priceChangePercentage24H: json["price_change_percentage_24h"]?.toDouble(),
+        priceChangePercentage24H:
+            json["price_change_percentage_24h"]?.toDouble(),
         marketCapChange24H: json["market_cap_change_24h"]?.toDouble(),
-        marketCapChangePercentage24H: json["market_cap_change_percentage_24h"]?.toDouble(),
+        marketCapChangePercentage24H:
+            json["market_cap_change_percentage_24h"]?.toDouble(),
         circulatingSupply: json["circulating_supply"]?.toDouble(),
         totalSupply: json["total_supply"]?.toDouble(),
         maxSupply: json["max_supply"]?.toDouble(),
@@ -85,9 +87,9 @@ class Coin {
         atlChangePercentage: json["atl_change_percentage"]?.toDouble(),
         atlDate: DateTime.parse(json["atl_date"]),
         lastUpdated: DateTime.parse(json["last_updated"]),
-    );
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "symbol": symbol,
         "name": name,
@@ -113,5 +115,37 @@ class Coin {
         "atl_change_percentage": atlChangePercentage,
         "atl_date": atlDate.toIso8601String(),
         "last_updated": lastUpdated.toIso8601String(),
-    };
+      };
+
+  static Coin testCoin = Coin.fromJson(
+    {
+      "id": "bitcoin",
+      "symbol": "btc",
+      "name": "Bitcoin",
+      "image":
+          "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+      "current_price": 28581,
+      "market_cap": 552563063360,
+      "market_cap_rank": 1,
+      "fully_diluted_valuation": 599302215038,
+      "total_volume": 18020419226,
+      "high_24h": 28538,
+      "low_24h": 27722,
+      "price_change_24h": 119.31,
+      "price_change_percentage_24h": 0.41921,
+      "market_cap_change_24h": 1733875552,
+      "market_cap_change_percentage_24h": 0.31478,
+      "circulating_supply": 19362225.0,
+      "total_supply": 21000000.0,
+      "max_supply": 21000000.0,
+      "ath": 69045,
+      "ath_change_percentage": -58.67218,
+      "ath_date": "2021-11-10T14:24:11.849Z",
+      "atl": 67.81,
+      "atl_change_percentage": 41980.99161,
+      "atl_date": "2013-07-06T00:00:00.000Z",
+      "roi": null,
+      "last_updated": "2023-05-02T14:44:22.847Z"
+    },
+  );
 }
