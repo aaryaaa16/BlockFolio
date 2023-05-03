f1d5f991c2da7602371dbc1634eec558ecfadfce
crpatil1901 <crpatil1901@gmail.com>
2023-05-03 14:09:06 +0530

    Connected Firebase'

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@ import 'package:block_folio/themes/color_scheme.dart';
 import 'package:block_folio/view_models/coins_viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Provider.debugCheckInvalidValueType = null;
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const BlockfolioApp());
 }
 
